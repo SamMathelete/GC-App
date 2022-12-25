5d8061aedf350c5b2a42239a6bc111a45b663dd0
Ayush Tiwari <84335414+ayushtiwari110@users.noreply.github.com>
2022-12-26 01:29:51 +0530

    added cardview component for schedule screen

---
+++ data/events.ts
@@ -0,0 +1,37 @@
+export const DUMMY_TECH_EVENTS = [
+    { name: "Capture the Flag", date: "12th March", venue: "LBC"},
+    { name: "Web Hackathon", date: "13th March", venue: "Online"},
+    { name: "AstroStars", date: "14th March", venue: "SES"},
+    { name: "Robowars", date: "15th March", venue: "SAC"},
+    { name: "Fincobizz", date: "16th March", venue: "Online"},
+    { name: "Codetion", date: "17th March", venue: "LBC"},
+    { name: "TechNotion", date: "18th March", venue: "SES"},
+    { name: "roboSoccer", date: "19th March", venue: "SBS"},
+    { name: "Software Dev", date: "20th March", venue: "LBC"},
+    { name: "UXtivity", date: "21th March", venue: "SAC"},
+]
+
+export const DUMMY_CULTURAL_EVENTS = [
+    { name: "Singing", date: "12th March", venue: "MHR Ground"},
+    { name: "Dancing", date: "13th March", venue: "Online"},
+    { name: "Writing", date: "14th March", venue: "SES"},
+    { name: "music", date: "15th March", venue: "SAC"},
+    { name: "dance", date: "16th March", venue: "Online"},
+    { name: "Lights", date: "17th March", venue: "LBC"},
+    { name: "Camera", date: "18th March", venue: "SES"},
+    { name: "Action", date: "19th March", venue: "SBS"},
+    { name: "Display", date: "20th March", venue: "LBC"},
+]
+
+export const DUMMY_SPORTS_EVENTS = [
+    { name: "Football", date: "12th March", venue: "LBC"},
+    { name: "Volleyball", date: "13th March", venue: "Online"},
+    { name: "Table Tennis", date: "14th March", venue: "SES"},
+    { name: "Cricket", date: "15th March", venue: "SAC"},
+    { name: "Badminton", date: "16th March", venue: "Online"},
+    { name: "Squ", date: "17th March", venue: "LBC"},
+    { name: "Basketball", date: "18th March", venue: "SES"},
+    { name: "Tennis", date: "19th March", venue: "SBS"},
+    { name: "Chess", date: "20th March", venue: "LBC"},
+    { name: "Carrom", date: "21th March", venue: "SAC"},
+]
