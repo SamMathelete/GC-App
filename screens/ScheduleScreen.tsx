c0eb546a2d1a0f1afda62dcd35d2ef1174c8fa29
Ayush Tiwari <84335414+ayushtiwari110@users.noreply.github.com>
2022-12-26 01:28:42 +0530

    Updated ScheduleScreen and added CardView Component

--- screens/ScheduleScreen.tsx
+++ screens/ScheduleScreen.tsx
@@ -1,13 +1,49 @@
 import { FC } from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { FlatList, ImageBackground, ScrollView, StyleSheet, Text, View } from "react-native";
 
+import CardView from "../components/CardView";
+import { DUMMY_TECH_EVENTS, DUMMY_CULTURAL_EVENTS, DUMMY_SPORTS_EVENTS } from "../data/events";
 const ScheduleScreen: FC = () => {
   return (
-    <ImageBackground
+      <ImageBackground
       source={require("../assets/Images/bg.jpg")}
       style={styles.rootContainer}
+      resizeMode="cover"
     >
-      <Text>Schedule Screen</Text>
+      <ScrollView style={styles.rootContainer}>
+        <View style={styles.rootContainer}>
+          <Text style={styles.eventHeading}>Tech Council</Text>
+          <View style={styles.eventList}>
+          <FlatList
+              data={DUMMY_TECH_EVENTS}
+              renderItem={({item}) => <CardView eventInfo={item} />}
+              horizontal={true}
+          />
+          </View>  
+        </View>
+        <View style={styles.rootContainer}>
+          <Text style={styles.eventHeading}>Cultural Council</Text>
+          <View style={styles.eventList}>
+          <FlatList
+              data={DUMMY_CULTURAL_EVENTS}
+              renderItem={({item}) => 
+              <CardView eventInfo={item} />}
+              horizontal={true}
+          />
+          </View>  
+        </View>
+        <View style={styles.rootContainer}>
+          <Text style={styles.eventHeading}>Sports Council</Text>
+          <View style={styles.eventList}>
+          <FlatList
+              data={DUMMY_SPORTS_EVENTS}
+              renderItem={({item}) => 
+              <CardView eventInfo={item} />}
+              horizontal={true}
+          />
+          </View>  
+        </View>
+      </ScrollView>
     </ImageBackground>
   );
 };
@@ -17,7 +53,16 @@ export default ScheduleScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingVertical: 10,
+  },
+  eventHeading: {
+    fontSize: 24,
+    color: "#FEDC86",
+    paddingStart: 10,
+    fontWeight: "bold",
+  },
+  eventList: {
+    backgroundColor: "#FFB968",
+    paddingVertical: 10,
   },
 })
